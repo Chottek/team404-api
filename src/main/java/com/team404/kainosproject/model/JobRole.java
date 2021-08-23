76eaaf891a680011f397c50dbc919c8e82d2c1ec
Jack Walton <jackrwal@gmail.com>
2021-08-23 15:03:20 +0100

    Commited Filled Job Role model

--- src/main/java/com/team404/kainosproject/model/JobRole.java
+++ src/main/java/com/team404/kainosproject/model/JobRole.java
@@ -1,2 +1,35 @@
-package com.team404.kainosproject.model;public class JobRole {
+package com.team404.kainosproject.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="job_role")
+public class JobRole {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name="job_id")
+    private Integer id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="contractType")
+    private String contractType;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
