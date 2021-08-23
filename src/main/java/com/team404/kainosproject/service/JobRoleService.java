8c939e0832c52d7b67f21a6ade2fe259f94ce270
BartoszCh <bartosz.chotkowski@kainos.com>
2021-08-23 17:28:27 +0200

    Added Logging to service and comment to controller

--- src/main/java/com/team404/kainosproject/service/JobRoleService.java
+++ src/main/java/com/team404/kainosproject/service/JobRoleService.java
@@ -2,11 +2,18 @@ package com.team404.kainosproject.service;
 
 import com.team404.kainosproject.model.JobRole;
 import com.team404.kainosproject.repository.JobRoleRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.stream.StreamSupport;
+
+
 @Service
 public class JobRoleService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JobRoleService.class);
 
     private final JobRoleRepository repository;
 
@@ -15,8 +22,14 @@ public class JobRoleService {
         this.repository = repository;
     }
 
+    /**
+     * Gets a List of JobRole objects from database
+     * @return JobRole objects list
+     */
     public Iterable<JobRole> getAll(){
-        return repository.findAll();
+        Iterable<JobRole> jobRoles = repository.findAll();
+        LOG.info("Got {} from database", StreamSupport.stream(jobRoles.spliterator(), false).count());
+        return jobRoles;
     }
 
 
