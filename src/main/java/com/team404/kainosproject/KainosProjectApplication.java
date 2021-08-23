e4de2b2837fe66b17ddb62df196001ae2db79acd
BartoszCh <bartosz.chotkowski@kainos.com>
2021-08-23 12:57:08 +0200

    Inital commit

---
+++ src/main/java/com/team404/kainosproject/KainosProjectApplication.java
@@ -0,0 +1,13 @@
+package com.team404.kainosproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KainosProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KainosProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team404/kainosproject/KainosProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team404.kainosproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KainosProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
