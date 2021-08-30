91c11ea8fdecd92ca6076db62ed121395713f7d9
SamuelChorvat <36892499+SamuelChorvat@users.noreply.github.com>
2021-08-30 15:56:06 +0100

    remove @SpringBootTest

--- src/test/java/com/team404/kainosproject/KainosProjectApplicationTests.java
+++ src/test/java/com/team404/kainosproject/KainosProjectApplicationTests.java
@@ -2,9 +2,7 @@ package com.team404.kainosproject;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertTrue;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class KainosProjectApplicationTests {
 
 	@Test
