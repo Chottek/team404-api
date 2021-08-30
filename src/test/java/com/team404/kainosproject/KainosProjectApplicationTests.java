c653f370f122b3ae78a06cbe1866218fa655c9f5
SamuelChorvat <36892499+SamuelChorvat@users.noreply.github.com>
2021-08-30 15:12:53 +0100

    add dummy test

--- src/test/java/com/team404/kainosproject/KainosProjectApplicationTests.java
+++ src/test/java/com/team404/kainosproject/KainosProjectApplicationTests.java
@@ -1,13 +1,15 @@
 package com.team404.kainosproject;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertTrue;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KainosProjectApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testAutomaticTestsOnPushMergeToMain() {
+		assertTrue(true);
 	}
 
 }
