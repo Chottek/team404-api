a43327ff89d0e0c92065087db25c97f601b387b5
BartoszCh <bartosz.chotkowski@kainos.com>
2021-08-31 09:59:28 +0200

    Changed mapping in RestController to a little bit more clear

--- src/main/java/com/team404/kainosproject/controller/BandController.java
+++ src/main/java/com/team404/kainosproject/controller/BandController.java
@@ -22,7 +22,7 @@ public class BandController {
         return service.getAllBands();
     }
 
-    @GetMapping("/bands-remapped")
+    @GetMapping("/bandswcompetencies")
     public Iterable<BandDTO> getBandDTOs(){
         return service.getAllBandsDTOs();
     }
