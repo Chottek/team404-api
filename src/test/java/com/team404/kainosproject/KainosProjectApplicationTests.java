b5d6c160aa103752207ea156d0551f9f6e41d730
SamuelChorvat <36892499+SamuelChorvat@users.noreply.github.com>
2021-08-31 11:08:07 +0100

    reformat KainosProjectApplicationTests

--- src/test/java/com/team404/kainosproject/KainosProjectApplicationTests.java
+++ src/test/java/com/team404/kainosproject/KainosProjectApplicationTests.java
@@ -1,7 +1,8 @@
 package com.team404.kainosproject;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 class KainosProjectApplicationTests {
 
