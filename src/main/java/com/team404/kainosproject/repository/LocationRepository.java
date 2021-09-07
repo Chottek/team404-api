bfac0ff2e4a37526b95e5ea7c50cd8157f9a69e0
BartoszCh <bartosz.chotkowski@kainos.com>
2021-09-07 17:05:10 +0200

    Added Locations functionalities for proper UI-side verification

--- src/main/java/com/team404/kainosproject/repository/LocationRepository.java
+++ src/main/java/com/team404/kainosproject/repository/LocationRepository.java
@@ -1,10 +1,16 @@
 package com.team404.kainosproject.repository;
 
 import com.team404.kainosproject.model.Location;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocationRepository extends CrudRepository<Location, Integer> {
+
+  String FIND_NAMES = "SELECT DISTINCT name FROM location";
+
+  @Query(value = FIND_NAMES, nativeQuery = true)
+  Iterable<String> getAllLocationNames();
 
 }
