ccfd2569ca37aed8fa937f79a6e69e144428ea25
BartoszCh <bartosz.chotkowski@kainos.com>
2021-09-09 17:51:35 +0200

    Fighting with an indentation error

--- src/main/java/com/team404/kainosproject/controller/JobRoleController.java
+++ src/main/java/com/team404/kainosproject/controller/JobRoleController.java
@@ -40,7 +40,7 @@ public class JobRoleController {
    *
    * @param id Numeric id of Job Role in database
    * @return ResponseEntity containing an object if it exists,
-   *  else ResponseEntity with 404 Not Found Status
+   *    else ResponseEntity with 404 Not Found Status
    */
   @GetMapping("/job-roles/{id}")
   public ResponseEntity<JobRoleDto> getById(@PathVariable("id") int id) {
