181baca2a9ff2a6b50b87143d066bbc53ff99338
BartoszCh <bartosz.chotkowski@kainos.com>
2021-09-09 17:54:01 +0200

    Fix linting checks (final)

--- src/main/java/com/team404/kainosproject/controller/JobRoleController.java
+++ src/main/java/com/team404/kainosproject/controller/JobRoleController.java
@@ -39,8 +39,7 @@ public class JobRoleController {
    * Gets Job Role object based on ID.
    *
    * @param id Numeric id of Job Role in database
-   * @return ResponseEntity containing an object if it exists,
-   *    else ResponseEntity with 404 Not Found Status
+   * @return ResponseEntity with an object if it exists,ResponseEntity with 404 Not Found Status else
    */
   @GetMapping("/job-roles/{id}")
   public ResponseEntity<JobRoleDto> getById(@PathVariable("id") int id) {
